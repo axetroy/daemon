9e54263c22888297df26fcae97a4ef52a68a82eb
axetroy <axetroy.dev@gmail.com>
2020-08-08 10:29:44 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/axetroy/daemon
+
+go 1.14
--- main.go
+++ main.go
@@ -16,14 +16,28 @@ USAGE:
     $ daemon [option] [command]
 
 OPTIONS:
-    --help         show help (default: false)
-    --version      print the version (default: false)
+    --help         	show help (default: false)
+    --version      	print the version (default: false)
+    --stdout				Specify the output file of stdout (default: nil)
+    --stderr				Specify the output file of stderr (default: nil)
 
 EXAMPLE:
     $ daemon --help
     $ daemon --version
     $ daemon node server.js
 	`)
+}
+
+func ensureFile(file string) (*os.File, error) {
+	if _, err := os.Stat(file); err != nil {
+		if os.ErrNotExist == err {
+			return os.Create(file)
+		} else {
+			return nil, err
+		}
+	} else {
+		return os.Open(file)
+	}
 }
 
 const VERSION = "v1.0.0"
@@ -34,12 +48,16 @@ Usage:
 */
 func main() {
 	var (
-		help    bool
-		version bool
+		help       bool
+		version    bool
+		stdoutFile string
+		stderrFile string
 	)
 
 	flag.BoolVar(&help, "help", false, "print help information")
 	flag.BoolVar(&version, "version", false, "print version information")
+	flag.StringVar(&stdoutFile, "stdout", "", "the output file of stdout")
+	flag.StringVar(&stderrFile, "stderr", "", "the output file of stderr")
 
 	flag.Parse()
 
@@ -80,6 +98,25 @@ func main() {
 		args := flag.Args()
 
 		cmd := exec.Command(args[0], args[1:]...)
+
+		if stdoutFile != "" {
+			if file, err := ensureFile(stderrFile); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				cmd.Stdout = file
+			}
+		}
+
+		if stderrFile != "" {
+			if file, err := ensureFile(stderrFile); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				cmd.Stderr = file
+			}
+		}
+
 		if err := cmd.Start(); err != nil {
 			log.Fatalln(err)
 		} else {
